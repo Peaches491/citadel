12858270b55e6b84e67a87394ad586587ee4438f
Daniel Miller <daniel@zoox.com>
2015-08-28 03:04:28 -0400

    Fixed accordion animation border rendering

--- app/includes.php
+++ app/includes.php
@@ -4,6 +4,7 @@ $js_sources = array(
   'external' => array(
     'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js',
     'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js',
+    'https://cdn.rawgit.com/pimvdb/jquery.chromelog/4c84617cbcddbc5dff2e5768b831ee8d17d01c82/jquery.chromelog.js'
   ),
   'internal' => array()
 );
